afdeba677cba7d726e323770929d9be677703b81
Jascha <jascha@jaeh.at>
2014-08-27 06:53:15 +0200

    jaeh.at assets first draft finished

---
+++ person.js
@@ -0,0 +1,73 @@
+"use strict";
+
+module.exports = {
+	firstname: "Jascha",
+	lastname: "Ehrenreich",
+	image: "jascha.ehrenreich.png",
+	jobTitle: "Webprogrammer and Streetartist",
+	slogan: "Magically shifting paradigms",
+	description: [
+		"i juggle with code since ten,",
+		"with cones, balls and rings since twenty",
+		"and life juggles with me since around thirty years."
+	],
+	affiliations: [
+		{
+			name: "Bitcoiners Without Borders",
+			url: "https://bwb.is",
+			logo: "bwb.png",
+			description: "We are a tribe of individuals tired of the destructive trip, workin' on a better Day.",
+			showtitle: true,
+		},
+		{
+			name: "Metalab",
+			url: "https://metalab.at",
+			logo: "metalab.png",
+			description: "The Metalab is the first Hackerspace in Vienna. I can usually be found in the Lounge.",
+			showtitle: true,
+		},
+	],
+	emails: [
+		'jascha@jaeh.at',
+		'jascha@bwb.is'
+	],
+	socialnetworks: [
+		{
+			text: "jascha",
+			logo: "social/keybase.png",
+			url: "https://keybase.io/jascha",
+			description: "my keybase profile"
+		},
+		{
+			text: "@weirdpress",
+			logo: "social/twitter.png",
+			url: "https://twitter.com/weirdpress",
+			description: "my twitter account"
+		},
+		{
+			text: "about.me",
+			logo: "social/aboutme.png",
+			url: "https://about.me/jascha.ehrenreich",
+			description: "my about.me page"
+		},
+		{
+			text: "suppe. ja. eh.",
+			logo: "social/soup.png",
+			url: "http://suppe.jaeh.at",
+			description: "soup (german: die suppe) is a multi content social blogging network."
+		},
+		{
+			text: "linkedin",
+			logo: "social/linkedin.png",
+			url: "https://at.linkedin.com/in/jaschaehrenreich",
+			description: "linkedin"
+		},
+		{
+			text: "facebook",
+			logo: "social/facebook.png",
+			url: "https://facebook.com/jascha.ehrenreich/",
+			description: "you all know the kraken."
+		},
+	]
+};
+					
---
+++ settings.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+	person: {
+		firstname: "Jascha",
+		lastname: "Ehrenreich",
+		image: "jascha.ehrenreich.png",
+		jobTitle: "Webprogrammer and Streetartist",
+		description: [
+			"i juggle with code since ten,",
+			"with cones, balls and rings since twenty",
+			"and life juggles with me since around thirty years."
+		],
+		emails: [
+			'jascha@jaeh.at',
+			'jascha@bwb.is'
+		],
+		socialnetworks: [
+			{
+				text: "jascha",
+				href: "https://keybase.io/jascha",
+				description: "my keybase profile"
+			},
+			{
+				text: "@weirdpress",
+				href: "https://twitter.com/weirdpress",
+				description: "my twitter account"
+			},
+			{
+				text: "about.me",
+				href: "https://about.me/jascha.ehrenreich",
+				description: "my twitter account"
+			},
+			{
+				text: "suppe. ja. eh.",
+				href: "http://suppe.jaeh.at",
+				description: "soup (german: die suppe) is a multi content social blogging network."
+			},
+			{
+				
+				text: "facebook",
+				href: "https://facebook.com/jascha.ehrenreich/",
+				description: "you all know the kraken."
+			}
+		]
+	}
+};
+						
